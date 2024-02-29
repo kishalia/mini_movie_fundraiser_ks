4dd4266ab6b2f2870ee7b11fd50432f5244a4089
kishalia <158999987+kishalia@users.noreply.github.com>
2024-03-01 10:23:01 +1300

    Create C07_string_checker.py
    Checks if user entered a valid response based on a list of options.

---
+++ C07_string_checker.py
@@ -0,0 +1,31 @@
+# checks that users enter a valid response (eg yes / no
+# cash / credit ) based on a list of options
+
+def string_checker(question, num_letters, valid_responses):
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_responses:
+            if response == item[0] or response == item:
+                return item
+
+        print("Please enter a valid response.")
+
+
+# main routine starts here
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
+
+for case in range(0, 5):
+    instructions = string_checker("Do you want to read the "
+                                  "instructions (y/n): ",
+                                  1, yes_no_list)
+
+    print("You chose", instructions)
+
+for case in range(0, 5):
+    pay_method = string_checker("Please choose a payment method"
+                                "(c/cr): ",
+                                2, payment_list)
+    print("You chose", pay_method)
